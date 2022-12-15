99eeaefec8fbd19d02c8fe5c0ecb0f6212c0894e
AlliahBulalacao <116430363+AlliahBulalacao@users.noreply.github.com>
2022-12-16 01:25:13 +0800

    Printing messages Dequeue

--- heap.py
+++ heap.py
@@ -1,16 +1,17 @@
 from heapq import heappush
+from heapq import heappop
+from queues import PriorityQueue
+
 
 colors = []
 heappush(colors, "red")
 heappush(colors, "apple")
 heappush(colors, "banana")
 
-print(fruits)
+print(colors)
 
-from heapq import heappop
-
-print(heappop(fruits))
-print(fruits)
+print(heappop(colors))
+print(colors)
 
 # another example
 person1 = ("Joey", "Legend", 39)
@@ -19,3 +20,19 @@ person3 = ("Ross", "Green", 29)
 
 print(person1 < person2)
 print(person2 < person3)
+
+
+CRITICAL = 3
+IMPORTANT = 2
+NEUTRAL = 1
+
+messages = PriorityQueue()
+messages.enqueue_with_priority(IMPORTANT, "Windshield wipers turned on")
+messages.enqueue_with_priority(NEUTRAL, "Radio station tuned in")
+messages.enqueue_with_priority(CRITICAL, "Brake pedal depressed")
+messages.enqueue_with_priority(IMPORTANT, "Hazard lights turned on")
+
+print(messages.dequeue())
+print(messages.dequeue())
+print(messages.dequeue())
+print(messages.dequeue())
